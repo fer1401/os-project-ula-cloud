72cca1bfdbeb3df582f8dd18b0ae2876daa50bfa
fer1401 <ferrujano.14@gmail.com>
2026-04-24 14:39:18 -0400

    feat:  restricción de recursos del sistema (apply_resource_limits) implementada

--- src/resources.c
+++ src/resources.c
@@ -12,9 +12,23 @@
  * * Ayuda: Revisar la constante RLIMIT_AS en la documentación de Linux.
  */
 void apply_resource_limits(size_t mem_limit) {
+    struct rlimit rl;
+
+    rl.rlim_cur = mem_limit; // Límite soft
+    rl.rlim_max = mem_limit; // Límite hard
+
+    if(setrlimit(RLIMIT_AS, &rl) == -1) 
+    {
+        perror("Error al establecer límites de recursos");
+        exit(EXIT_FAILURE);
+    }
+    
     // TODO: Configurar la estructura rlimit y ejecutar la syscall.
     
     // Casos a considerar:
     // - ¿Qué diferencia hay entre rlim_cur y rlim_max?
+    //rlim_cur (soft), limite actual que el proceso puede usar y sobrepasar (quedando "vivo" pero con una advertencia)
+    //rlim_max (hard), limite máximo que el proceso puede usar pero no sobrepasar (no lanza advertencia, "mata" el proceso directamente)
     // - ¿Qué sucede si el límite solicitado es menor al tamaño del propio binario?
+    //si el limite es menor al tamaño del binario, el proceso no puede cargar por completo la imagen en memoria(lanza error de memoria insuficiente)
 }
