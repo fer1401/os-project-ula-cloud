317fd66c5fea4de035aab3918f6c800489deebf9
fer1401 <ferrujano.14@gmail.com>
2026-04-24 18:41:42 -0400

    feat: rutina del hilo monitor implementada (monitor_service)

--- src/monitor.c
+++ src/monitor.c
@@ -15,8 +15,40 @@
 void* monitor_service(void *arg) {
     // TODO: Castear el argumento al tipo de dato correcto.
     
+    service_t *svc = (service_t *)arg;
+
+    int status;
+
     // TODO: Implementar la espera del proceso específico.
     // Ayuda: Revisar el uso de waitpid(pid, &status, 0).
+
+    //le paso como ultimo argumento 0 (porque no hay opciones)
+    //si no hay opciones el proceso padre debe esperar a que el proceso hijo termine para continuar con la ejecución del siguiente código 
+    waitpid(svc->pid, &status, 0); 
+
+   if(WIFEXITED(status))
+   {
+        //si el proceso terminó normalmente, actualizo el estado a STOPPED y guardo el código de salida
+        pthread_mutex_lock(&dashboard_mutex);
+        svc->state = STATE_STOPPED;
+        svc->exit_status = WEXITSTATUS(status);
+        pthread_mutex_unlock(&dashboard_mutex);
+   }
+   else if(WIFSIGNALED(status))
+   {
+        //si el proceso fue terminado por una señal, actualizo el estado a KILLED y guardo la señal que lo mató
+        pthread_mutex_lock(&dashboard_mutex);
+        svc->state = STATE_KILLED;
+        svc->exit_status = WTERMSIG(status);
+        pthread_mutex_unlock(&dashboard_mutex);
+   }
+   else
+   {
+        //en cualquier otro caso, lo marco como CRASHED (puede ser un caso raro pero es para cubrir todas las posibilidades)
+        pthread_mutex_lock(&dashboard_mutex);
+        svc->state = STATE_CRASHED;
+        pthread_mutex_unlock(&dashboard_mutex);
+   }
 
     /* * Una vez que waitpid retorna, el proceso hijo ha cambiado de estado.
      * TODO: Analizar el 'status' usando las macros de sys/wait.h:
